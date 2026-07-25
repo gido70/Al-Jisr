e3afbe9dfc6ac3984da1cdcb72870cd888468b31
gido70 <63196885+gido70@users.noreply.github.com>
2026-07-25 14:59:12 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aljisr-v8';
+const CACHE = 'aljisr-v9';
 const CORE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
