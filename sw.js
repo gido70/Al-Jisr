2f1350de098f813f1ce29685dd9867fe6fe2d70e
gido70 <63196885+gido70@users.noreply.github.com>
2026-07-31 15:16:32 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aljisr-v2026-07-31-log-toolbar';
+const CACHE_NAME = 'aljisr-v20260731-live-modes';
 const CORE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => Promise.allSettled(CORE.map(u => cache.add(u)))).then(() => self.skipWaiting()));
