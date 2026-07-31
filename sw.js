2e613fc69bf7deaf36558408b2daebd6aa2686d7
gido70 <63196885+gido70@users.noreply.github.com>
2026-08-01 02:13:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aljisr-clean-v12';
+const CACHE_NAME = 'aljisr-mobile-a-to-z-v8';
 const CORE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => Promise.allSettled(CORE.map(u => cache.add(u)))).then(() => self.skipWaiting()));
