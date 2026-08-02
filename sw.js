111fd122a943ce6b6dd2fe3dc839b3375e4b6f8d
gido70 <63196885+gido70@users.noreply.github.com>
2026-08-03 00:48:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aljisr-v2026-08-02-repeat-and-ios-audio-fix';
+const CACHE_NAME = 'aljisr-v2026-08-02-android-reconnect-fix';
 const CORE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => Promise.allSettled(CORE.map(u => cache.add(u)))).then(() => self.skipWaiting()));
