de36dc52e5f1f38e7016a78de246d138a62ab218
gido70 <63196885+gido70@users.noreply.github.com>
2026-08-05 01:41:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aljisr-v2026-08-04-mic-visibility-safety-net';
+const CACHE_NAME = 'aljisr-v2026-08-04-fix-mic-root-cause-topbar2-moved-out';
 const CORE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => Promise.allSettled(CORE.map(u => cache.add(u)))).then(() => self.skipWaiting()));
