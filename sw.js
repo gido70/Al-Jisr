e9343ef36fef9362e179630ad2be320475bf647f
gido70 <63196885+gido70@users.noreply.github.com>
2026-08-08 18:22:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aljisr-v2026-08-08g-fix-growing-repeat';
+const CACHE_NAME = 'aljisr-v2026-08-08h-screen-wakelock';
 const CORE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => Promise.allSettled(CORE.map(u => cache.add(u)))).then(() => self.skipWaiting()));
