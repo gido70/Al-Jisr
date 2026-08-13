0d991cd1c2c93cfe2cc4c479dce8380e92542f6a
gido70 <63196885+gido70@users.noreply.github.com>
2026-08-13 19:05:03 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aljisr-v2026-08-13-lens-v2-ar-v2';
+const CACHE_NAME = 'aljisr-v2026-08-13-lenses-google-style-test';
 const CORE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => Promise.allSettled(CORE.map(u => cache.add(u)))).then(() => self.skipWaiting()));
